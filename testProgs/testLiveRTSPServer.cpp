7e34f08bc70cc7365948e0a9f5432fb08ab7a39d
sudoz <isudox@gmail.com>
2015-01-22 15:04:55 +0800

    live555 media server uploaded

--- testProgs/testLiveRTSPServer.cpp
+++ testProgs/testLiveRTSPServer.cpp
@@ -62,6 +62,61 @@ int main(int argc, char* argv[])
 	TaskScheduler* scheduler = BasicTaskScheduler::createNew();
 	env = BasicUsageEnvironment::createNew(*scheduler);
 
+	UserAuthenticationDatabase* authDB = NULL;
+
+	// 没有设定用户验证就跳过下面步骤
+#ifdef ACCESS_CONTROL
+	// To implement client access control to the RTSP server, do the following:
+	authDB = new UserAuthenticationDatabase;
+	authDB->addUserRecord("username1", "password1"); // replace these with real strings
+	// Repeat the above with each <username>, <password> that you wish to allow
+	// access to the server.
+#endif
+
+	// Create the RTSP server:
+	RTSPServer* rtspServer = RTSPServer::createNew(*env, 8554, authDB);
+	if (rtspServer == NULL) {
+		*env << "Failed to create RTSP server: " << env->getResultMsg() << "\n";
+		exit(1);
+	}
+
+	char const* descriptionString = "Session streamed by \"testLiveRTSPServer\"";
+	// Set up each of the possible streams that can be served by the
+	// RTSP server.  Each such stream is implemented using a
+	// "ServerMediaSession" object, plus one or more
+	// "ServerMediaSubsession" objects for each audio/video substream.
+
+	// A H.264 video elementary stream:
+	{
+		char const* streamName = "h264ESVideoTest";
+		char const* inputFileName = "test.264";	// TODO
+		ServerMediaSession* sms = ServerMediaSession::createNew(*env, streamName, descriptionString);
+		// 修改为自己实现的serverMedia
+		sms->addSubsession(H264LiveVideoServerMediaSubssion::createNew(*env, inputFileName, reuseFirstSource));
+		rtspServer->addServerMediaSession(sms);
+
+		announceStream(rtspServer, sms, streamName, inputFileName);
+	}
+
+	// TODO 其他编码类型按需添加
+
+	// Also, attempt to create a HTTP server for RTSP-over-HTTP tunneling.
+	// Try first with the default HTTP port (80), and then with the alternative HTTP
+	// port numbers (8000 and 8080).
+
+	env->taskScheduler().doEventLoop(); // does not return
+
+	return 0; // only to prevent compiler warning
 }
 
+static void announceStream(RTSPServer* rtspServer, ServerMediaSession* sms,
+		char const* streamName, char const* inputFileName)
+{
+	char* url = rtspServer->rtspURL(sms);	// 获取服务器URL
+	UsageEnvironment& env = rtspServer->envir();
+	env << "\n\"" << streamName << "\" stream, from the file \""
+			<< inputFileName << "\"\n";
+	env << "Play this stream using the URL \"" << url << "\"\n";
 
+	delete[] url;
+}
